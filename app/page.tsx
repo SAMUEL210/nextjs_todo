04a9e00b27d27231d24db2ec99b110eaeaccb9b0
simarone <samuel210@live.fr>
2024-10-03 16:05:15 +0200

    dependencies installed and button added project going on

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div className="max-w-7x1 mx-auto p-10">
-      <h1 className="text-4x1 font-bold">Todos</h1>
+    <div className="max-w-7x1 flex-col gap-10 mx-auto p-10">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4x1 font-bold">TÂCHES</h1>
+        <Button>Ajouter des taches</Button>
+      </div>
     </div>
   );
 }
