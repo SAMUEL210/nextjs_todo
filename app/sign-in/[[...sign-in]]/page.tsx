044b5488cd0cc1cb83b2cb4edd74ebe32dcc01b5
samuel210 <samuel210@live.fr>
2025-04-25 14:51:01 +0200

    removing file

--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs'
-
-export default function Page() {
-    return (
-        <div className='w-full h-full flex flex-row justify-center items-center mt-20'>
-            <SignIn />
-        </div>
-    )
-}
--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs'
-
-export default function Page() {
-    return (
-        <div className='w-full h-full flex flex-row justify-center items-center mt-15'>
-            <SignUp />
-        </div>
-    )
-}
