93d9d8426827494af99049fe86259e0f63f53456
samuel210 <samuel210@live.fr>
2025-04-25 14:54:19 +0200

    updating todos schema

--- lib/zod.ts
+++ lib/zod.ts
@@ -4,6 +4,7 @@ export const todoSchema = z.object({
     title: z.string().min(1, "Le titre est requis!").max(100, "Le titre ne doit pas dépasser 100 caractères"),
     description: z.string().max(500, "La description de doit pas dépasser 500 caractères").optional(),
     isCompleted: z.boolean().default(false),
+    userId: z.string()
 })
 
 export type TodoSchema = z.infer<typeof todoSchema>;
