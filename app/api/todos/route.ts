fab46a99a60ab9e2b9e824698ac8d3eb0aefa764
samuel210 <samuel210@live.fr>
2025-04-25 16:20:27 +0200

    updating api to handle user auth and userId for filtering todos

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,77 +1,106 @@
 import prisma from '@/lib/prisma';
-
 import { todoSchema } from '@/lib/zod';
-
 import { Todo } from '@prisma/client';
-
 import { NextRequest, NextResponse } from 'next/server';
 
-
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+ 
 export async function GET(){
-    try {
-        const todos = await prisma.todo.findMany({
-            orderBy: {
-                createdAt: 'desc',
-            },
-        });
-        return NextResponse.json(todos);
-    }catch (error){
-        console.error('Erreur lors du chargement: ', error);
-        return NextResponse.json({message: "Une erreur innattendu s'est produit"}, {status: 500});      
+    const session = await auth.api.getSession({
+        headers: headers()
+    })
+    if(session != null){
+        try {
+            const todos = await prisma.todo.findMany({
+                where:{
+                    userId: session.user.id
+                },
+                orderBy: {
+                    createdAt: 'desc',
+                },
+            });
+            return NextResponse.json(todos);
+        }catch (error){
+            console.error('Erreur lors du chargement: ', error);
+            return NextResponse.json({message: "Une erreur innattendu s'est produit"}, {status: 500});      
+        }
+    }else{
+        return NextResponse.json({error: 'Veuillez vous identifier!'})
     }
+    
 }
 
 export async function POST( request: NextRequest){
-    try{
-        const body = await request.json();
-        const result = todoSchema.safeParse(body);
-        
-        if(!result.success){
-            return NextResponse.json({ message:"Champs invalid", errors: result.error.errors}, {status: 400})
-        }
-
-        const todoData = result.data;
-        const newTodo = await prisma.todo.create({
-            data: {
-                title: todoData.title,
-                description: todoData.description || '',
-                isCompleted: todoData.isCompleted,
+    const session = await auth.api.getSession({
+        headers: headers()
+    })
+    if(session != null){
+        try{
+            const body = await request.json();
+            const result = todoSchema.safeParse(body);
+            
+            if(!result.success){
+                return NextResponse.json({ message:"Champs invalid", errors: result.error.errors}, {status: 400})
             }
-        });
-
-        return NextResponse.json(newTodo, {status: 201});
-
-    }catch(error){
-        console.error('Erreur lors de la creation: ', error);
-        return NextResponse.json({message: "Une erreur innattendu s'est produit"}, {status: 500});
+    
+            const todoData = result.data;
+            const newTodo = await prisma.todo.create({
+                data: {
+                    title: todoData.title,
+                    userId: todoData.userId,
+                    description: todoData.description || '',
+                    isCompleted: todoData.isCompleted,
+                }
+            });
+    
+            return NextResponse.json(newTodo, {status: 201});
+    
+        }catch(error){
+            console.error('Erreur lors de la creation: ', error);
+            return NextResponse.json({message: "Une erreur innattendu s'est produit"}, {status: 500});
+        }
+    }else{
+        return NextResponse.json({error: 'Veuillez vous identifier!'})
     }
 }
 
 export async function DELETE(request: NextRequest){
-    try{
-        const id = request.nextUrl.searchParams.get('id');
-        if(!id){
-            return NextResponse.json({message: "Id de la tâche est requise!"}, {status: 400});
+    const session = await auth.api.getSession({
+        headers: headers()
+    })
+    if(session != null){
+        try{
+            const id = request.nextUrl.searchParams.get('id');
+            if(!id){
+                return NextResponse.json({message: "Id de la tâche est requise!"}, {status: 400});
+            }
+
+            const deleteTodo = await prisma.todo.delete({
+                where: {id},
+            });
+
+            if(!deleteTodo){
+                return NextResponse.json({message: "Tâche non trouvé"}, {status: 404});
+            }
+
+            return NextResponse.json({message: "Tâche supprimé avec succès"}, {status: 200});
+
+        }catch(error){
+            console.error("Erreur lors de la suppression de la tâche : ", error);
+            return NextResponse.json({message: "Une erreur inattendu s'est produite"}, {status: 500});
         }
-
-        const deleteTodo = await prisma.todo.delete({
-            where: {id},
-        });
-
-        if(!deleteTodo){
-            return NextResponse.json({message: "Tâche non trouvé"}, {status: 404});
-        }
-
-        return NextResponse.json({message: "Tâche supprimé avec succès"}, {status: 200});
-
-    }catch(error){
-        console.error("Erreur lors de la suppression de la tâche : ", error);
-        return NextResponse.json({message: "Une erreur inattendu s'est produite"}, {status: 500});
+    }else{
+        return NextResponse.json({error: 'Veuillez vous identifier!'})
     }
 }
 
 export async function PUT(request: NextRequest) {
-    try{
+    const session = await auth.api.getSession({
+        headers: headers()
+    })
+    if(session != null){
+        try{
         const body =await request.json();
         const {id, ...rest} = body;
 
@@ -101,8 +130,11 @@ export async function PUT(request: NextRequest) {
 
         return  NextResponse.json(updateTodo, {status: 200});
     
-    }catch(error){
-        console.error("Erreur lors de la modification de la tâche : ", error);
-        return NextResponse.json({message: "Une erreur inattendu s'est produite"}, {status: 500});
-    }  
+        }catch(error){
+            console.error("Erreur lors de la modification de la tâche : ", error);
+            return NextResponse.json({message: "Une erreur inattendu s'est produite"}, {status: 500});
+        }
+    }else{
+        return NextResponse.json({error: 'Veuillez vous identifier!'})
+    }
 }
