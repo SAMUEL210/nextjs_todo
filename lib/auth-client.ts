918b7d6f69373e1fd27755f71605a0755bb34f36
samuel210 <samuel210@live.fr>
2025-04-29 16:51:14 +0200

    corrections

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://taches.samuelmarone.fr"
+    baseURL: "https://taches.samuelmarone.fr"
 })
 
 export const { signIn, signOut, signUp, useSession } = authClient;
