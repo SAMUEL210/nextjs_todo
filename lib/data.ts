b2a6feb5e54ab1d32fcbf014870a080195494099
samuel210 <samuel210@live.fr>
2025-05-01 15:05:46 +0200

    New data structure

---
+++ lib/data.ts
@@ -0,0 +1,14 @@
+export const states =  [
+  {
+    id: 0,
+    state: "undone",
+    title: "TACHES",
+    description: "Liste de vos tâches à faire!"
+  },
+  {
+    id: 1,
+    state: "done",
+    title: "TACHES FAITES",
+    description: "Liste de vos tâches que vous avez déjà accomplie"
+  }
+]
