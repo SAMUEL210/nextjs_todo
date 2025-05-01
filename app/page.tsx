9b0d6a614be02e92242c4b5136d23ba15eb9ad73
samuel210 <samuel210@live.fr>
2025-05-01 15:06:30 +0200

    adding new data structure and new todos listing structure

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,9 @@ import CreateTodo from '@/components/create-todo';
 import TodoList from '@/components/todo-list';
 import { authClient } from "@/lib/auth-client" // import the auth client
 import { redirect } from 'next/navigation';
-
-const states = [
-  {
-    state: "undone",
-    title: "TACHES"
-  },
-  {
-    state: "done",
-    title: "TACHES FAITES"
-  }
-];
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { states } from '@/lib/data';
 
 export default function Home() {
   const { data: session, isPending } = authClient.useSession()
@@ -23,25 +15,35 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-7x1 flex-col gap-10 mx-auto p-10">
+    <div className="w-10/12 flex-col gap-10 mx-auto p-10">
       <div className="flex justify-between items-center">
         {(isPending == false && session != null) &&
           <CreateTodo userId={session.user.id} />
         }
-
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>{
-        (states) &&
-        states.map((state) => (
-          <div key={state.state}>
-            <h1 className="text-4x1 font-bold pb-5">{state.title}</h1>
-            <div className='p-5 border border-1 border-black rounded-lg'>
-              <TodoList todoState={state.state} />
-            </div>
-          </div>
-        ))
-      }
-      </div>
+      <Tabs defaultValue="undone" className="max-w-xxl mx-auto mt-2">
+        <TabsList className="grid w-full grid-cols-2 text-white bg-green-800">
+          {(states) && states.map((state) => (
+            <TabsTrigger key={state.id} value={state.state}>{state.title}</TabsTrigger>
+          ))
+          }
+        </TabsList>
+        {(states) && states.map((state) => (
+          <TabsContent key={state.id} value={state.state}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{state.title}</CardTitle>
+                <CardDescription>
+                  {state.description}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <TodoList todoState={state.state} />
+              </CardContent>
+            </Card>
+          </TabsContent>
+        ))}
+      </Tabs>
     </div>
-  );
+  )
 }
