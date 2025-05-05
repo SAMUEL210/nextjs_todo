a0877f97201a625c72f6a2cb5f419f5b75be2920
samuel210 <samuel210@live.fr>
2025-05-05 18:04:36 +0200

    correction on mail sender

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,18 +1,16 @@
 import { createTransport } from "nodemailer";
 
-export const mailer = createTransport(
-    {   
-        host: "smtp-marone.alwaysdata.net",
-        port: 465,
-        secure: true,
-        dkim:{
-            domainName: "samuelmarone.fr",
-            keySelector: "2017",
-            privateKey: process.env.PRIVATE_KEY,
-        },
-        auth: {
-            user: "contact.taaches@samuelmarone.fr",
-            pass: "jn7jnAPss4f63QBp6D",
-        },
-    }
-)
+export const mailer = createTransport({
+    host: "smtp-marone.alwaysdata.net",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "contact.taaches@samuelmarone.fr",
+        pass: "jn7jnAPss4f63QBp6D",
+    },
+    dkim: {
+        domainName: "samuelmarone.fr",
+        keySelector: "2017",
+        privateKey: process.env.PRIVATE_KEY || "",
+    },
+} as import("nodemailer").TransportOptions);
