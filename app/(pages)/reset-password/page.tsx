1f3727c1a9e6ef7b4c3f87ffbc7e94c5265447fd
samuel210 <samuel210@live.fr>
2025-05-08 16:10:22 +0200

    correction on reset password handler the page display on existence of token

--- app/(pages)/reset-password/page.tsx
+++ app/(pages)/reset-password/page.tsx
@@ -9,34 +9,33 @@ export default function ResetPassword() {
     const searchParams = useSearchParams();
     const token = searchParams.get('token');
 
-    return (
-        (token) ? (
-            <div className="flex min-h-[50vh] h-full w-full items-center justify-center px-4" >
-                <Card className="mx-auto max-w-sm">
-                    <CardHeader>
-                        <CardTitle className="text-2xl">Réinitialisation de mot de passe</CardTitle>
-                        <CardDescription>
-                            Veuillez entrer votre nouveau mot de passe ci-dessous.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <ResetPasswordForm
-                            token={token}
-                        />
-                    </CardContent>
-                </Card>
-            </div>
-        ) : (
+    if (token) {
+        return (
             <div className="flex min-h-[50vh] h-full w-full items-center justify-center px-4">
                 <Card className="mx-auto max-w-sm">
                     <CardHeader>
                         <CardTitle className="text-2xl">Réinitialisation de mot de passe</CardTitle>
                         <CardDescription>
-                            Vous n&apos;êtes pas autorisé à accéder à cette page. Veuillez passer par le lien de réinitialisation de mot de passe que vous avez reçu par e-mail.
+                            Veuillez entrer votre nouveau mot de passe.
                         </CardDescription>
                     </CardHeader>
+                    <CardContent>
+                        <ResetPasswordForm token={token} />
+                    </CardContent>
                 </Card>
             </div>
         )
+    }
+    return (
+        <div className="flex min-h-[50vh] h-full w-full items-center justify-center px-4">
+            <Card className="mx-auto max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Réinitialisation de mot de passe</CardTitle>
+                    <CardDescription>
+                        Vous n&apos;êtes pas autorisé à accéder à cette page. Veuillez passer par le lien de réinitialisation de mot de passe que vous avez reçu par e-mail.
+                    </CardDescription>
+                </CardHeader>
+            </Card>
+        </div>
     )
 }
