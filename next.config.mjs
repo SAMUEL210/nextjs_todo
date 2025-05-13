cd0d527301aa2289f9a33f9b570fbe1f63133fab
samuel210 <samuel210@live.fr>
2025-05-13 13:33:26 +0200

    corrections

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-      },
-      experimental: {
-        missingSuspenseWithCSRBailout: false,
       }
 };
 
