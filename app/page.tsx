c2f2905c24c98735eac92924fe988f8c40cad4b7
samuel210 <samuel210@live.fr>
2025-05-13 10:44:48 +0200

    home page modifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,11 @@
-'use client'
+import Navbar from "@/components/navbar"
+import { HeroSection } from "@/components/blocks/hero-section"
 
-import CreateTodo from '@/components/create-todo';
-import TodoList from '@/components/todo-list';
-import { authClient } from "@/lib/auth-client" // import the auth client
-import { redirect } from 'next/navigation';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { states } from '@/lib/data';
-
-export default function Home() {
-
-  const { data: session, isPending } = authClient.useSession()
-
-
+export default function HomePage() {
   return (
-    (isPending == false && session != null) ? (
-      <div className="w-10/12 flex-col gap-10 mx-auto p-10">
-        <div className="flex justify-between items-center">
-          {(isPending == false && session != null) &&
-            <CreateTodo userId={session.user.id} />
-          }
-        </div>
-        <Tabs defaultValue="undone" className="max-w-xxl mx-auto mt-2">
-          <TabsList className="grid w-full grid-cols-2 text-white bg-green-800">
-            {(states) && states.map((state) => (
-              <TabsTrigger key={state.id} value={state.state}>{state.title}</TabsTrigger>
-            ))
-            }
-          </TabsList>
-          {(states) && states.map((state) => (
-            <TabsContent key={state.id} value={state.state}>
-              <Card>
-                <CardHeader>
-                  <CardTitle>{state.title}</CardTitle>
-                  <CardDescription>
-                    {state.description}
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <TodoList todoState={state.state} />
-                </CardContent>
-              </Card>
-            </TabsContent>
-          ))}
-        </Tabs>
-      </div>
-    ) : redirect('/login')
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
   )
 }
